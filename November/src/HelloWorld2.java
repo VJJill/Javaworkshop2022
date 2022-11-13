38ecb6edbca94bca12b21d52835528a2e90aa6b2
vj <jvrao405@gmail.com>
2022-11-13 08:05:21 +0530

    changes

--- November/src/HelloWorld2.java
+++ November/src/HelloWorld2.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HelloWorld2 {
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        //Print statement - For commit
+        System.out.println("Hello World2");
+    }
 }
